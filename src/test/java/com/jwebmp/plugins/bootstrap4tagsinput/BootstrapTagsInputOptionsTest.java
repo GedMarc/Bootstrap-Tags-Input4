b3da02974d8f359ec23b9d60461c0db9392738f9
GedMarc <ged_marc@hotmail.com>
2019-01-17 22:00:45 +0200

    GPG Update

--- src/test/java/com/jwebmp/plugins/bootstrap4tagsinput/BootstrapTagsInputOptionsTest.java
+++ src/test/java/com/jwebmp/plugins/bootstrap4tagsinput/BootstrapTagsInputOptionsTest.java
@@ -3,7 +3,7 @@ package com.jwebmp.plugins.bootstrap4tagsinput;
 import org.junit.jupiter.api.Test;
 
 /**
- * @author Marc Magon
+ * @author GedMarc
  */
 public class BootstrapTagsInputOptionsTest
 {
