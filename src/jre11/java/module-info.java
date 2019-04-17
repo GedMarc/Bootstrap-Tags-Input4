278dfea157fb4ebdbe4e37e2f3ba4e35dfe1e3ff
GedMarc <ged_marc@hotmail.com>
2019-04-17 04:09:42 +0200

    DevKit and Structure with Cleanup

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,6 +1,3 @@
-import com.jwebmp.plugins.bs4.tagsinput.BS4TagsInputPageConfigurator;
-import com.jwebmp.plugins.bs4.tagsinput.implementations.BS4TagsInputExclusionsModule;
-
 module com.jwebmp.plugins.bs4.tagsinput {
 	exports com.jwebmp.plugins.bs4.tagsinput;
 
@@ -12,10 +9,10 @@ module com.jwebmp.plugins.bs4.tagsinput {
 	requires java.validation;
 	requires com.jwebmp.guicedinjection;
 
-	provides com.jwebmp.core.services.IPageConfigurator with BS4TagsInputPageConfigurator;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.bs4.tagsinput.BS4TagsInputPageConfigurator;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with BS4TagsInputExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with BS4TagsInputExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.bs4.tagsinput.implementations.BS4TagsInputExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.bs4.tagsinput.implementations.BS4TagsInputExclusionsModule;
 
 	opens com.jwebmp.plugins.bs4.tagsinput to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
